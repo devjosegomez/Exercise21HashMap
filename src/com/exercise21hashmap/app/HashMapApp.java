82dc4f65ef91e10f1843398101b4a7881badbee2
devjosegomez <devjosegomez@gmail.com>
2019-10-23 12:56:19 -0500

    Example of Hash map

--- src/com/exercise21hashmap/app/HashMapApp.java
+++ src/com/exercise21hashmap/app/HashMapApp.java
@@ -1,9 +1,21 @@
 package com.exercise21hashmap.app;
-
+import java.util.HashMap;
+import java.util.Map;
 public class HashMapApp {
 
 	public static void main(String[] args) {
+		HashMap<Integer, String> myHash = new HashMap<Integer, String>();
+		myHash.put(1, "Jose");
+		myHash.put(2, "Angela");
+		for(int i=0; i<myHash.size(); i++) {
+			if(myHash.containsKey(i)) {
+				System.out.println(myHash.get(i));
+			}
+		}
 		
+		for(Map.Entry<Integer,String> myMap : myHash.entrySet()) {
+			System.out.println(myMap);
+		}
 	}
 
 }
